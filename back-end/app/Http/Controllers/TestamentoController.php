7e747fe8becc5fe77b0bd7cf290f1fcf68f57b0b
Gimenez <gabrielgimenez09@gmail.com>
2023-02-20 01:55:12 -0300

    Commit base do projeto.

---
+++ back-end/app/Http/Controllers/TestamentoController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Testamento;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class TestamentoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): Response
+    {
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request): Response
+    {
+        return Testamento::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id): Response
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id): Response
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id): Response
+    {
+        //
+    }
+}
